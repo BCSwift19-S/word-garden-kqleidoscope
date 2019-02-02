6bc26b3895203e7c4b34dfee61b59863964e0936
Hiroki <ll@lls-macbook-pro.local>
2019-02-02 15:51:52 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Hiroki on 2019/02/02.
+//  Copyright © 2019 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
